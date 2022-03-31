3a715809beabba48f1b2a527c714301a4c8d07eb
Daniel Suarez <suarez.daniel101@gmail.com>
2022-03-31 17:15:58 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 require './config/environment'
+require "./app/models/user"
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  get "/signup" do
+    erb :signup
   end
 
 end
