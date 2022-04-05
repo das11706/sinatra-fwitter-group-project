ed719fba002b8ab281bb3d17de9e98c27790953c
Daniel Suarez <suarez.daniel101@gmail.com>
2022-04-05 17:58:26 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,28 @@
 class UsersController < ApplicationController
 
   get "/signup" do
+    if !logged_in?
     erb :'/users/create_user'
+    else 
+      redirect to "/tweets"
+    end
   end
 
     
   post "/signup" do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
     user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    user.save
       session[:user_id] = user.id 
     # binding.pry
     # if !user.username.empty? && !user.email.empty? && !user.password.empty?
     # user = User.find_by(:username => params[:username])
-    if user.save
+    # if user.save
+    #   redirect "/tweets"
+    # else
       redirect "/tweets"
-    else
-      redirect "/signup"
     end
   end
 
