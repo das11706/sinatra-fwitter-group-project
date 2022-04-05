a99f943d4c8fd7d02227adc9a3916ae4eb8ddd05
Daniel Suarez <suarez.daniel101@gmail.com>
2022-04-04 21:12:50 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,32 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get "/signup" do
-    erb :signup
+  # get "/signup" do
+  #   erb :signup
+  # end
+
+  # post "/signup" do
+  #   user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+  #   # binding.pry
+  #     if user.save
+  #       redirect "/tweets"
+  #     else
+  #       redirect "/signup"
+  #     end
+  # end
+
+  # get '/tweets' do
+  #   "Hello World"
+  # end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
 
-  
+  get "/signup" do
+    erb :'/users/create_user'
+  end
+
+    
+  post "/signup" do
+    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        # binding.pry
+    if user.save
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+  helpers do
+    def logged_in?
+     !!session[:user_id]
+    end  
+    
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
