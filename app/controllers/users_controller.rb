a36596ea7aa3cea9274076322e24db3baa8a2a90
Daniel Suarez <suarez.daniel101@gmail.com>
2022-04-04 22:46:33 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   post "/signup" do
     user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
         # binding.pry
+    # if !user.username.empty? && !user.email.empty? && !user.password.empty?
     if user.save
       redirect "/tweets"
     else
