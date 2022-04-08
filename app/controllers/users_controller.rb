383641566a9589bd1ed32fe0a455e406a538ebc8
Daniel Suarez <suarez.daniel101@gmail.com>
2022-04-08 16:48:11 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,53 +1,50 @@
 class UsersController < ApplicationController
 
-  # get "/signup" do
-  #   if !logged_in?
-  #   erb :'/users/create_user'
-  #   else 
-  #     redirect to "/tweets"
-  #   end
-  # end
+  get "/signup" do
+    if !logged_in?
+    erb :'/users/create_user'
+    else 
+      redirect to "/tweets"
+    end
+  end
 
-    
-  # post "/signup" do
-  #   if params[:username] == "" || params[:email] == "" || params[:password] == ""
-  #     redirect to '/signup'
-  #   else
-  #   user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-  #   user.save
-  #   session[:user_id] = user.id 
-  #     redirect "/tweets"
-  #   end
-  # end
+  post "/signup" do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user.save
+    session[:user_id] = @user.id 
+      redirect "/tweets"
+    end
+  end
 
-  # get "/login" do
-  #   erb :'/users/login'
-  # end
+  get "/login" do
+    if !logged_in?
+    erb :'users/login'
+    else
+      redirect to "/tweets"
+    end
+  end
 
-  # post "/login" do
-  #   user = User.find_by(:username => params[:username])
-  #   # binding.pry
-  #   if user && user.authenticate(params[:password])
-  #     session[:user_id] = user.id 
-  #   redirect "/tweets/tweets"
-  # #  else
-  # #    redirect "/failure"
-  #   end
-  # end
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    # binding.pry
+    # if @user && @user.authenticate(params[:password])
+    if @user
+      session[:user_id] = @user.id 
+    redirect to "/tweets"
+   else
+     redirect to "/login"
+    end
+  end
 
-  # get "/tweets" do
-  #   user = User.find(session[:user_id])
-  #   erb :'/tweets'
-  # end
-
-
-  helpers do
-    def logged_in?
-     !!session[:user_id]
-    end  
-    
-    def current_user
-      User.find(session[:user_id])
+  get "/logout" do
+    if logged_in?
+      session.destroy
+      redirect to "/login"
+    else  
+      redirect to "/"
     end
   end
 
