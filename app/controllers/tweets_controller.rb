7827390b2c051285c323a06e41753b3398317035
Daniel Suarez <suarez.daniel101@gmail.com>
2022-04-08 16:48:31 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,8 @@ class TweetsController < ApplicationController
   get "/tweets" do
     @tweets = Tweet.all
     if !logged_in?
-    #   erb :'/users/login'
-      redirect to "/login"
+      erb :'/users/login'
+    #   redirect to "/login"
     else
       erb :'/tweets/tweets'
     end
