c36952cbddac166b3ca1751b5ad82c46643a2cf6
Daniel Suarez <suarez.daniel101@gmail.com>
2022-04-16 22:15:29 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -22,9 +22,13 @@ class TweetsController < ApplicationController
   end
  
   get "/tweets/:id" do
+    if logged_in?
     @tweet = Tweet.find_by_id(params[:id])
     # binding.pry
     erb :'/tweets/show_tweet'
+    else
+      redirect to "/login"
+    end
   end
 
   post "/tweets" do
@@ -38,9 +42,32 @@ class TweetsController < ApplicationController
     end
   end
 
-  # get "/tweets/:slug" do
-  #   @tweet = Tweet.find_by_slug(params[:slug])
-  #   erb :'/tweets/show_tweet'
-  # end
+  get "/tweets/:id/edit" do
+    if logged_in?
+      @tweet = Tweet.find_by(params[:id])
+    erb :'/tweets/edit_tweet'
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/tweets/:id" do
+    @tweet = Tweet.find_by(params[:id])
+    erb :'/tweets/show_tweet'
+  end
+
+  patch '/tweets/:id' do
+    if logged_in?
+    @tweet = Tweet.find_by(params[:id])
+    # @tweet.user.update(params["user"])
+    @tweet.update(params[:tweet])
+    # binding.pry
+    redirect to "/tweets/#{@tweet.id}/edit"
+    else
+      redirect "/login"
+    end
+  end
 
 end
+
+
